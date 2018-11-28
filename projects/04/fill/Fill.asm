e9216d0023cc1c1ab1d54e41d202211bf2625ca0
zakimal <oza777you@gmail.com>
2018-11-28 22:24:06 +0900

    Complete Chapter 4

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,50 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+	@status 	// A = status
+	M = -1		// RAM[status] = -1
+	D = 0		// D = 0
+	@SETSCREEN	// A = SETSCREEN
+	0; JMP		// jump to SETSCREEN
+
+(LOOP)
+	@KBD		// A = KBD
+	D = M 		// D = RAM[KBD]
+	@SETSCREEN	// A = SETSCREEN
+	D; JEQ 		// if D == 0 then jump to SETSCREEN
+	D = -1		// D = -1
+
+(SETSCREEN)
+	@ARG 		// A = R2
+	M = D 		// RAM[2] = D
+	@status 	// A = status
+	D = D - M 	// D = D - RAM[status]
+	@LOOP 		// A = LOOP
+	D; JEQ 		// if D == 0 then jump tp LOOP
+
+	@ARG 		// A = R2
+	D = M 		// D = RAM[2]
+	@status 	// A = status
+	M = D 		// RAM[status] = D
+
+	@SCREEN 	// A = SCREEN
+	D = A 		// D = SCREEN
+	@8192 		// A = 8192
+	D = D + A   // D = D + 8192
+	@i 			// A = i
+	M = D 		// RAM[i] = D
+
+(SETLOOP)
+	@i 			// A = i
+	D = M - 1 	// D = RAM[i] - 1
+	M = D 		// RAM[i]
+	@LOOP 		// A = LOOP
+	D; JLT 		// if D < 0 then jump LOOP
+
+	@status 	// A = status
+	D = M 		// D = RAM[status]
+	@i 			// A = i
+	A = M 		// A = RAM[i]
+	M = D 		// RAM[i] = D
+	@SETLOOP 	// A = SETLOOP
+	0; JMP      // jump to SETLOOP
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -5,5 +5,42 @@
 
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
-
+// R0, R1, R2 is addresses.
 // Put your code here.
+
+// @value means "load the value to the A register."
+// and a value on the memory referenced by M is the one which address is the value A register holds.
+
+//
+// int RAM[3] = 0
+// while (i - RAM[1] < 0) {
+// 		RAM[2] += RAM[0];
+// 		RAM[3]++;
+// }
+//
+
+// initialize R2
+@R2 			// A = R2
+M = 0 			// RAM[2] = 0
+
+@R3 			// A = R3
+M = 0 			// RAM[3] = 0
+
+(LOOP)
+	@R3 		// A = R3
+	D = M 		// D = RAM[3]
+	@R1 		// A = R1
+	D = D - M 	// RAM[3] = RAM[3] - RAM[1]
+	@END 		// A = &END
+	D; JGE 		// if D (= RAM[3] - RAM[1]) >= 0 then jump to END
+	@R0 		// A = R0
+	D = M 		// D = RAM[0]
+	@R2 		// A = R2
+	M = M + D 	// RAM[2] = RAM[2] + RAM[0]
+	@R3 		// A = R3
+	M = M + 1 	// RAM[R3] = RAM[R3]  + 1
+	@LOOP 		// A = &LOOP
+	0; JMP 		// jump to LOOP
+(END)
+	@END 		// A = &END
+	0; JMP 		// jump to END
